e1f0265809088f415b00ba6ac19df28342e85a0b
Jingqiu Chen <monhuntqq@gmail.com>
2022-10-23 03:07:05 +1000

    Version 4 Traj_core 1. Fixing scaling issue: previously no scale applyed no matter 3,5,7,                          also takes in T as total time and scale each                          second of the movement 2. Rename to perform better
    Version 1 Self_def_IK_core by Richard
    1. Functionality: good

---
+++ Richard_IK.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def IKinSpace(x,y,z):
+    L1 = 100
+    L2 = 117.5
+    L3 = 95
+    L4 = 110
+
+    j0 = np.arctan2(y,x)
+    L=x/np.cos(j0)
+    for theta2 in range(0,90):
+        j1 = theta2 / 180 * np.pi
+     
+        Lc = L2 * np.cos(j1)
+        Ls = L2 * np.sin(j1)
+      
+        beta = np.arctan2(L-Ls,L1+Lc)
+        L5 = np.sqrt((L-Ls)**2+(L1+Lc-z)**2)
+        ac1 = (L3**2+L4**2-L5**2)/(2*L3*L4)
+        ac2 = (L3**2+L5**2-L4**2)/(2*L3*L5)
+        '''
+        print(ac1)
+        print(ac2)
+        '''
+
+        if -1<ac1<1 and -1<ac2<1:
+            j3 = np.pi-np.arccos(ac1)
+            j2 = np.pi-j1-beta-np.arccos(ac2)
+            A=L*np.cos(j0)
+            B=L*np.sin(j0)
+            C=L1+L2*np.cos(j1)+L3*np.cos(j1+j2)+L4*np.cos(j1+j2+j3)
+            print(j0,j1,j2,j3,A,B,C)
+        else:
+            pass
+        '''
+        while -1<ac1<1 and -1<ac2<1:
+            j3 = np.pi-np.arccos(ac1)
+            j2 = np.pi-j1-beta-np.arccos(ac2)
+            A=L*np.cos(j0)
+            B=L*np.sin(j0)
+            C=L1+L2*np.cos(j1)+L3*np.cos(j1+j2)+L4*np.cos(j1+j2+j3)
+
+            if A==x and B==y and C==z:
+                print(theta1, theta2, theta3, theta4)
+            else:
+                pass
+        '''
--- trajectory_generation/scripts/Traj_core.py
+++ trajectory_generation/scripts/Traj_core.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+from re import T
 import numpy as np
 
-def d2r(deg):
-    """Simply change degree angle to radius angle
-    """
-    rad = deg/180*np.pi
-    return rad
+# def d2r(deg):
+#     """Simply change degree angle to radius angle
+#     """
+#     rad = deg/180*np.pi
+#     return rad
 
 def third_poly_scale(t,T):
     """Scale the time domain using 3rd order polynomial
+
+       Input: mapping t seconds to s domain in T seconds, sample on each sec, t[0,T] to s[0,1]
     """
     s = 3 * (t/T)**2 - 2 * (t/T)**3
     return s
@@ -25,158 +28,193 @@ def seventh_poly_scale(t,T):
     s = 35 * (t/T)**4 - 84 * (t/T)**5 + 70 * (t/T)**6 - 20 * (t/T)**7
     return s
 
-def joint_validation(joint_ans):
-    """Validate all joint answers to select the best one to generate joint
-       trajecoy
 
-       Input: joint_ans: theta lists in array form
+# def joint_validation(joint_ans):
+#     """Validate all joint answers to select the best one to generate joint
+#        trajecoy
 
-       Implementation for now: Validate answers with angle limits then only rewrite [i] answer with [i+1] answer
+#        Input: joint_ans: theta lists in array form
+
+#        Implementation for now: Validate answers with angle limits then only rewrite [i] answer with [i+1] answer
+#     """
+#     N = len(joint_ans)
+
+#     first_valid = np.zeros((N, 4))
+    
+#     for i in range(N):
+#         joint_angle_1 = joint_ans[i, 0]
+#         joint_angle_2 = joint_ans[i, 1]
+#         joint_angle_3 = joint_ans[i, 2]
+#         joint_angle_4 = joint_ans[i, 3]
+        
+#         if d2r(-40) <= joint_angle_1 <= d2r(40) and 0 <= joint_angle_2 <= d2r(90) and 0 <= joint_angle_3 <= d2r(150) and 0 <= joint_angle_4 <= (np.pi - joint_angle_2 - joint_angle_3):
+#             first_valid[i] = joint_ans[i]
+#         else:
+#             pass
+
+#     # n = 0
+    
+#     valid_ans = np.zeros((1,4))
+#     for i in range(N):
+#         if list(first_valid[i]) != [0,0,0,0]:
+#             valid_ans = first_valid[i]
+#         else:
+#             pass
+
+#     # if n == 1:
+#     #     valid_ans = np.delete((re_shape,
+#     #     return valid_ans
+#     # elif n > 1:
+#     #     valid_ans = np.zeros
+
+#     #return first_valid
+#     return valid_ans
+
+def joint_traj_gen(theta_end,T,scale):
+    """Generate trajectory from valid angle answers, sample at each second
     """
-    N = len(joint_ans)
-
-    first_valid = np.zeros((N, 4))
-    
-    for i in range(N):
-        joint_angle_1 = joint_ans[i, 0]
-        joint_angle_2 = joint_ans[i, 1]
-        joint_angle_3 = joint_ans[i, 2]
-        joint_angle_4 = joint_ans[i, 3]
-        if d2r(-40) <= joint_angle_1 <= d2r(40) and 0 <= joint_angle_2 <= d2r(90) and 0 <= joint_angle_3 <= d2r(150) and 0 <= joint_angle_4 <= (np.pi - joint_angle_2 - joint_angle_3):
-            first_valid[i] = joint_ans[i]
+    theta_start = np.array([0,0,0,0])
+    theta_s = np.zeros((T,4))
+    for i in range(T):
+        if scale == 3:
+            s = third_poly_scale(i+1,T)
+        elif scale == 5:
+            s = fifth_poly_scale(i+1,T)
+        elif scale == 7:
+            s = seventh_poly_scale(i+1,T)
         else:
-            pass
-
-    # n = 0
+            print("Please enter a proper scale method from 3,5,7")
+        theta_s[i] = theta_start + s * (theta_end - theta_start)
     
-    valid_ans = np.zeros((1,4))
-    for i in range(N):
-        if list(first_valid[i]) != [0,0,0,0]:
-            valid_ans = first_valid[i]
-        else:
-            pass
-
-    # if n == 1:
-    #     valid_ans = np.delete((re_shape,
-    #     return valid_ans
-    # elif n > 1:
-    #     valid_ans = np.zeros
-
-    #return first_valid
-    return valid_ans
-
-def joint_traj_gen(theta_start,theta_end,t,T,scale):
-    """Generate trajectory from valid angle answers, set the robot to move
-       5 seconds, one angle change per second
-    """
-    if scale == 3:
-        s = third_poly_scale(t,T)
-    elif scale == 5:
-        s = fifth_poly_scale(t,T)
-    elif scale == 7:
-        s = seventh_poly_scale(t,T)
-    else:
-        print("Please enter a proper scale method from 3,5,7")
+    # theta_s[0] = theta_start + s * (theta_start - theta_end) * (1/T)
+    # theta_s[1] = theta_start + s * (theta_start - theta_end) * (2/T)
+    # theta_s[2] = theta_start + s * (theta_start - theta_end) * (3/T)
+    # theta_s[3] = theta_start + s * (theta_start - theta_end) * (4/T)
+    # theta_s[4] = theta_start + s * (theta_start - theta_end) * (5/T)
     
-    theta_s = np.zeros((6,4))
-    theta_s[0] = theta_start
-    theta_s[1] = theta_start + s * (theta_start - theta_end) * 0.2
-    theta_s[2] = theta_start + s * (theta_start - theta_end) * 0.4
-    theta_s[3] = theta_start + s * (theta_start - theta_end) * 0.6
-    theta_s[4] = theta_start + s * (theta_start - theta_end) * 0.8
-    theta_s[5] = theta_start + s * (theta_start - theta_end) * 1
 
     return theta_s
 
-def back2home_traj(theta_end):
-    """Generate trejactory from current pose to home pose, allow robot to move within 5 sec
+def back2home_traj_gen(theta_end,T,scale):
+    """Generate trejactory from current pose to home pose, sample at each second
     """
-    joint_traj = joint_traj_gen(np.array([0,0,0,0]),theta_end,5,5,5)
-    theta_b2h = np.zeros((6,4))
+    joint_traj = joint_traj_gen(theta_end,T,scale)
+    theta_b2h = np.zeros((T,4))
+    for i in range(T-1):
+        theta_b2h[i] = joint_traj[T-2-i]
     
-    theta_b2h[0] = joint_traj[5]
-    theta_b2h[1] = joint_traj[4]
-    theta_b2h[2] = joint_traj[3]
-    theta_b2h[3] = joint_traj[2]
-    theta_b2h[4] = joint_traj[1]
-    theta_b2h[5] = np.array([0,0,0,0])
+    # theta_b2h[0] = joint_traj[3]
+    # theta_b2h[1] = joint_traj[2]
+    # theta_b2h[2] = joint_traj[1]
+    # theta_b2h[3] = joint_traj[0]
+    # theta_b2h[4] = theta_start
+    
     
     return theta_b2h
 
-def home2zone(zone):
+def home2zone_traj_gen(theta_end,T,scale):
     """Generate trajectory from home pose to required zone
     """
-    zone1 = np.asarray([[   0,    0,  0,    0],
-                        [21.6,  4.2, 14, 15.6],
-                        [43.2,  8.4, 28, 31.2],
-                        [64.8, 12.6, 42, 46.8],
-                        [86.4, 16.8, 56, 62.4],
-                        [ 108,   21, 70,   78]])
-    zone2 = np.asarray([[    0,    0,  0,    0],
-                        [ 32.4,  4.2, 14, 15.6],
-                        [ 64.8,  8.4, 28, 31.2],
-                        [ 97.2, 12.6, 42, 46.8],
-                        [129.6, 16.8, 56, 62.4],
-                        [  162,   21, 70,   78]])
-    zone3 = np.asarray([[    0,    0,  0,    0],
-                        [-21.6,  4.2, 14, 15.6],
-                        [-43.2,  8.4, 28, 31.2],
-                        [-64.8, 12.6, 42, 46.8],
-                        [-86.4, 16.8, 56, 62.4],
-                        [ -108,   21, 70,   78]])
-    zone4 = np.asarray([[     0,    0,  0,    0],
-                        [ -32.4,  4.2, 14, 15.6],
-                        [ -64.8,  8.4, 28, 31.2],
-                        [ -97.2, 12.6, 42, 46.8],
-                        [-129.6, 16.8, 56, 62.4],
-                        [  -162,   21, 70,   78]])
+    theta_start = np.array([0,0,0,0])
+    theta_h2z = np.zeros((T,4))
+    for i in range(T):
+        if scale == 3:
+            s = third_poly_scale(i+1,T)
+        elif scale == 5:
+            s = fifth_poly_scale(i+1,T)
+        elif scale == 7:
+            s = seventh_poly_scale(i+1,T)
+        else:
+            print("Please enter a proper scale method from 3,5,7")
+        theta_h2z[i] = theta_start + s * (theta_end - theta_start)
+    
+    # theta_s = np.zeros((6,4))
+    
+    # theta_s[0] = theta_start
+    # theta_s[1] = theta_start + s * (theta_start - theta_end) * 0.2
+    # theta_s[2] = theta_start + s * (theta_start - theta_end) * 0.4
+    # theta_s[3] = theta_start + s * (theta_start - theta_end) * 0.6
+    # theta_s[4] = theta_start + s * (theta_start - theta_end) * 0.8
+    # theta_s[5] = theta_start + s * (theta_start - theta_end) * 1
+    
+    return theta_h2z
+    
+    # zone1 = np.asarray([[   0,    0,  0,    0],
+    #                     [21.6,  4.2, 14, 15.6],
+    #                     [43.2,  8.4, 28, 31.2],
+    #                     [64.8, 12.6, 42, 46.8],
+    #                     [86.4, 16.8, 56, 62.4],
+    #                     [ 108,   21, 70,   78]])
+    # zone2 = np.asarray([[    0,    0,  0,    0],
+    #                     [ 32.4,  4.2, 14, 15.6],
+    #                     [ 64.8,  8.4, 28, 31.2],
+    #                     [ 97.2, 12.6, 42, 46.8],
+    #                     [129.6, 16.8, 56, 62.4],
+    #                     [  162,   21, 70,   78]])
+    # zone3 = np.asarray([[    0,    0,  0,    0],
+    #                     [-21.6,  4.2, 14, 15.6],
+    #                     [-43.2,  8.4, 28, 31.2],
+    #                     [-64.8, 12.6, 42, 46.8],
+    #                     [-86.4, 16.8, 56, 62.4],
+    #                     [ -108,   21, 70,   78]])
+    # zone4 = np.asarray([[     0,    0,  0,    0],
+    #                     [ -32.4,  4.2, 14, 15.6],
+    #                     [ -64.8,  8.4, 28, 31.2],
+    #                     [ -97.2, 12.6, 42, 46.8],
+    #                     [-129.6, 16.8, 56, 62.4],
+    #                     [  -162,   21, 70,   78]])
 
-    if zone == 1:
-        return d2r(zone1)
-    elif zone == 2:
-        return d2r(zone2)
-    elif zone == 3:
-        return d2r(zone3)
-    elif zone == 4:
-        return d2r(zone4)
-    else:
-        print("Please select a proper drop zone 1,2,3,4")
+    # if zone == 1:
+    #     return d2r(zone1)
+    # elif zone == 2:
+    #     return d2r(zone2)
+    # elif zone == 3:
+    #     return d2r(zone3)
+    # elif zone == 4:
+    #     return d2r(zone4)
+    # else:
+    #     print("Please select a proper drop zone 1,2,3,4")
+    
 
-def zone2home(zone):
+def zone2home_traj_gen(theta_end,T,scale):
     """Generate trajectory from zone pose to home pose
     """
-    if zone == 1:
-        z2h = np.asarray([[ 108,   21, 70,   78],
-                          [86.4, 16.8, 56, 62.4],
-                          [64.8, 12.6, 42, 46.8],
-                          [43.2,  8.4, 28, 31.2],
-                          [21.6,  4.2, 14, 15.6],
-                          [   0,    0,  0,    0]])
-    elif zone == 2:
-        z2h = np.asarray([[  162,   21, 70,   78],
-                          [129.6, 16.8, 56, 62.4],
-                          [ 97.2, 12.6, 42, 46.8],
-                          [ 64.8,  8.4, 28, 31.2],
-                          [ 32.4,  4.2, 14, 15.6],
-                          [    0,    0,  0,    0]])
-    elif zone == 3:
-        z2h = np.asarray([[ -108,   21, 70,   78],
-                          [-86.4, 16.8, 56, 62.4],
-                          [-64.8, 12.6, 42, 46.8],
-                          [-43.2,  8.4, 28, 31.2],
-                          [-21.6,  4.2, 14, 15.6],
-                          [    0,    0,  0,    0]])
-    elif zone == 4:
-        z2h = np.asarray([[  -162,   21, 70,   78],
-                          [-129.6, 16.8, 56, 62.4],
-                          [ -97.2, 12.6, 42, 46.8],
-                          [ -64.8,  8.4, 28, 31.2],
-                          [ -32.4,  4.2, 14, 15.6],
-                          [     0,    0,  0,    0]])
-    else:
-        print("Please select a proper drop zone 1,2,3,4")
-    
-    return d2r(z2h)
+    # if zone == 1:
+    #     z2h = np.asarray([[ 108,   21, 70,   78],
+    #                       [86.4, 16.8, 56, 62.4],
+    #                       [64.8, 12.6, 42, 46.8],
+    #                       [43.2,  8.4, 28, 31.2],
+    #                       [21.6,  4.2, 14, 15.6],
+    #                       [   0,    0,  0,    0]])
+    # elif zone == 2:
+    #     z2h = np.asarray([[  162,   21, 70,   78],
+    #                       [129.6, 16.8, 56, 62.4],
+    #                       [ 97.2, 12.6, 42, 46.8],
+    #                       [ 64.8,  8.4, 28, 31.2],
+    #                       [ 32.4,  4.2, 14, 15.6],
+    #                       [    0,    0,  0,    0]])
+    # elif zone == 3:
+    #     z2h = np.asarray([[ -108,   21, 70,   78],
+    #                       [-86.4, 16.8, 56, 62.4],
+    #                       [-64.8, 12.6, 42, 46.8],
+    #                       [-43.2,  8.4, 28, 31.2],
+    #                       [-21.6,  4.2, 14, 15.6],
+    #                       [    0,    0,  0,    0]])
+    # elif zone == 4:
+    #     z2h = np.asarray([[  -162,   21, 70,   78],
+    #                       [-129.6, 16.8, 56, 62.4],
+    #                       [ -97.2, 12.6, 42, 46.8],
+    #                       [ -64.8,  8.4, 28, 31.2],
+    #                       [ -32.4,  4.2, 14, 15.6],
+    #                       [     0,    0,  0,    0]])
+    # else:
+    #     print("Please select a proper drop zone 1,2,3,4")
+    joint_traj = joint_traj_gen(theta_end,T,scale)
+    traj_z2h = np.zeros((T,4))
+    for i in range(T-1):
+        traj_z2h[i] = joint_traj[T-2-i]
+   
+    return traj_z2h
 
 def end_point_traj_gen(X_start,X_end,t,T,scale):
     """
